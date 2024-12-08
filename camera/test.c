7515dcea6e4a9a586471d7a1ef092e8e03479067
elem44 <celm12312@gmail.com>
2024-12-08 22:46:22 +0000

    Basic Camera to follow player

---
+++ camera/test.c
@@ -0,0 +1,95 @@
+// From movement
+#include <raylib.h>
+#include <raymath.h>
+
+typedef struct Player {
+    Vector2 position;
+    Vector2 velocity;
+    bool can_jump;
+} Player;
+
+typedef struct EnvRectangle {
+    Rectangle rect;
+} EnvRectangle;
+
+
+float rec_top(Rectangle rect);
+
+
+int main(void) {
+    int screen_width = 800;
+    int screen_height = 600;
+
+    int G = 10;
+
+    Player player = {
+        (Vector2){(float)screen_width / 2, (float)screen_height / 2},
+        {0, 0},
+        false,
+    };
+    Camera2D camera = { 0 };
+    camera.target = player.position;
+    camera.offset = (Vector2){ screen_width/2.0f, screen_height/2.0f };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+    EnvRectangle floor = {{50, 500, 700, 80}};
+
+    InitWindow(screen_width, screen_height, "movement");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+
+        if (IsKeyDown(KEY_LEFT)) 
+        {
+            player.velocity.x = -8;
+        } 
+        else if (IsKeyDown(KEY_RIGHT)) 
+        {
+            player.velocity.x = 8;
+        } 
+        else
+        {
+            player.velocity.x = 0;
+        }
+
+        if (player.can_jump)
+        {
+            if (IsKeyPressed(KEY_SPACE))
+            {
+                player.velocity.y -= 20;
+                player.can_jump = false;
+            }
+        }
+
+        player.velocity.y += (float)(G * 0.1);
+
+        player.position = Vector2Add(player.position, player.velocity);
+
+        if (CheckCollisionCircleRec(player.position, 40, floor.rect)) 
+        {
+            player.can_jump = true;
+            player.position.y = 460;
+            player.velocity.y = 0;
+        }
+
+        camera.target = player.position;
+
+        BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+
+            BeginMode2D(camera);
+            DrawCircleV(player.position, 40, BLUE);
+            DrawRectangleRec(floor.rect, GRAY);
+            EndMode2D();
+        EndDrawing();
+    }
+
+    CloseWindow();
+}
+
+float rec_top(Rectangle rect)
+{
+    return (float) rect.y - rect.height;
+}
